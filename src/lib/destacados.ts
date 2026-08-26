ffa1ab9dcf58252cde72368bbeb4cb1be4a7e7cf
desalix <diegui8008@gmail.com>
2026-08-26 14:49:28 +0200

    Cambios desde el panel

--- src/lib/destacados.ts
+++ src/lib/destacados.ts
@@ -3,4 +3,12 @@
  *  Se gestiona desde el panel, en «Cambiar destacados». Añadir o quitar a mano
  *  también vale: es el id de la entrada, o sea el nombre de su .txt sin la
  *  extensión. Un id que no exista rompe el build, así que las erratas se ven. */
-export const DESTACADOS: string[] = [];
+export const DESTACADOS: string[] = [
+	'ford-mustang-shelby-v8-5-2-gt500-454497',
+	'volvo-xc90-d5-inscription-awd-235-aut-876499',
+	'mercedes-benz-a-45-amg-s-4matic-8g-dct-318023',
+	'bmw-m4-104868',
+	'mercedes-benz-cl-500-be-aut-623987',
+	'mitsubishi-montero-3-2-di-d-gls-kaiteki-at-781704',
+	'pontiac-trans-am-285-cvs-v8-aut-historico-1993-440330',
+];
